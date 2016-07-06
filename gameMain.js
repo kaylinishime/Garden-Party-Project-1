1fade0f21fbfccf518c6670bcd75cdb5999b0823
Kayli Nishime <kmnishime@ucdavis.edu>
2016-07-06 16:54:23 -0700

    fixed level two bug

--- gameMain.js
+++ gameMain.js
@@ -3,43 +3,38 @@ console.log("jqeury connected");
 $(document).ready(function(){
 
 
-  var level;
+var level;
 
 //randomizes veggetables on farmland
-  var apple = "http://i.imgur.com/xuboL0V.jpg";
-  var avocado = "http://i.imgur.com/q6fYImz.jpg";
-  var strawberry = "http://i.imgur.com/u7GvQkK.jpg";
-  var snail = "http://i.imgur.com/XI5oAX7.jpg";
-  var watermelon = "http://i.imgur.com/d3BVON7.jpg";
-  var objs = [apple,avocado,strawberry,watermelon,snail];
-  var test = [];
+var apple = "http://i.imgur.com/xuboL0V.jpg";
+var avocado = "http://i.imgur.com/q6fYImz.jpg";
+var strawberry = "http://i.imgur.com/u7GvQkK.jpg";
+var snail = "http://i.imgur.com/XI5oAX7.jpg";
+var watermelon = "http://i.imgur.com/d3BVON7.jpg";
+var objs = [apple,avocado,strawberry,watermelon,snail];
+var test = [];
 
 //randomize veggies and place them in an empty array which makes the new land
 
-  function farm() {
-    for(var i = 0; i<= 80; i++) {
-      test.push(objs[Math.ceil(Math.random()*10) % 5]);
-    }
+function farm() {
+  for (var i = 0; i<= 80; i++) {
+    test.push(objs[Math.ceil(Math.random()*10) % 5]);
+  };
+  $('.image').each(function (i) {
+    $(this).append('<img src=' + test[i] + ' class="veggies" />');
+  });
+};
 
-    $('.image').each(function (i) {
-      $(this).append('<img src=' + test[i] + ' class="veggies" />');
-      });
-    }
-
-    function resetLand(){
-      $('.image').each(function (i) {
-        $(this).text("");
-        });
-    }
-
+function resetLand(){
+  $('.image').each(function (i) {
+    $(this).text("");
+  });
+};
 
 
-//Score point logic, how many points per crop clicked
+//score points assigned to each vegetable
 var score = 0;
-// TODO use jquery
-
 $('#land').on("click", "span", function(evt){
-  // console.log(evt.detail); // doesn't work with jquery
   if (evt.target.src === apple) {
     score = score + 1;
   } else if (evt.target.src === snail) {
@@ -65,40 +60,38 @@ $( evt.target ).hide( 500, function() {
 var timer;
 var minutes;
 var seconds;
+var interval;
 
 function startTimer(duration, display) {
    timer = duration, minutes, seconds;
 
    var interval = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.text(minutes + ":" + seconds);
+      display.text(minutes + ":" + seconds);
         if (score === 100 && level === "one"){
-          console.log("You win!");
-          timer = 0;
           clearInterval(interval);
           winLevelOne();
         }
         else if (score > 100 && level === "one"){
           clearInterval(interval);
-          score = 0;
           youLose();
         }
 
         if (score === 200 && level === "two"){
-          console.log("You win!");
-          timer = 0;
           clearInterval(interval);
           winLevelTwo();
         }
+        else if (score > 200 && level === "two"){
+          clearInterval(interval);
+          youLose();
+        }
         else if (score === 268 && level === "two"){
           console.log("You win!");
-          timer = 0;
-          clearInterval(interval);
           winLevelThree();
         }
         //
@@ -115,8 +108,6 @@ function startTimer(duration, display) {
         if (score === targetScore.levelOne && level === "one"){
           console.log("You win!");
         } else if (score !== targetScore.levelOne && level === "one"){
-          console.log ("You lose!");
-          clearInterval(interval);
           youLose();
           };
         }
@@ -139,11 +130,13 @@ $('#first').click(function() {
 
 //popup window - level 2
 function winLevelOne() {
+  timer = 0;
 $('#popupTwo').show();
 $('#second').click(function() {
   $('#popupTwo').hide();
-  test = [];
   startTimer(60, display);
+  test = [];
+  resetLand();
   level = "two";
   score = 0;
   farm();
@@ -151,14 +144,22 @@ $('#second').click(function() {
 }
 
 function winLevelTwo() {
+  timer = 0;
 $('#popupThree').show();
 $('#third').click(function() {
   $('#popupThree').hide();
   startTimer(60, display);
+  test = [];
+  resetLand();
+  level = "three";
+  score = 0;
+  farm();
 });
 }
 
 function winLevelThree() {
+  timer = 0;
+
 $('#popupThree').show();
 $('#third').click(function() {
   $('#popupThree').hide();
@@ -166,7 +167,9 @@ $('#third').click(function() {
 });
 }
 
+//When player loses, must restart to Level 1!
 function youLose(){
+  score = 0;
 $('.restartOne').show();
 $('#restart').click(function(){
   $('.restartOne').hide();
