937085d80f1409cdfc8495698742cbac66132d88
Kayli Nishime <kmnishime@ucdavis.edu>
2016-07-08 09:55:25 -0700

    updated levels to reset land

--- gameMain.js
+++ gameMain.js
@@ -1,9 +1,13 @@
-
 console.log("jqeury connected");
 // $(document).ready(function(){
 
 
 var level;
+//Clock countdown
+var timer;
+var minutes;
+var seconds;
+var interval;
 
 //randomizes veggetables on farmland
 var apple = "http://i.imgur.com/xuboL0V.jpg";
@@ -11,23 +15,23 @@ var avocado = "http://i.imgur.com/q6fYImz.jpg";
 var strawberry = "http://i.imgur.com/u7GvQkK.jpg";
 var snail = "http://i.imgur.com/XI5oAX7.jpg";
 var watermelon = "http://i.imgur.com/d3BVON7.jpg";
-var objs = [apple,avocado,strawberry,watermelon,snail];
+var objs = [apple, avocado, strawberry, watermelon, snail];
 var test = [];
 
 //randomize veggies and place them in an empty array which makes the new land
 
 function farm() {
-  for (var i = 0; i<= 100; i++) {
-    test.push(objs[Math.ceil(Math.random()*10) % 5]);
+  for (var i = 0; i <= 100; i++) {
+    test.push(objs[Math.ceil(Math.random() * 10) % 5]);
   };
-  $('.image').each(function (i) {
+  $('.image').each(function(i) {
     $(this).append('<img src=' + test[i] + ' class="veggies" />');
   });
 };
 
 //empties the array to create a new shuffle after every level or lose
-function resetLand(){
-  $('.image').each(function (i) {
+function resetLand() {
+  $('.image').each(function(i) {
     $(this).text("");
   });
 };
@@ -35,7 +39,7 @@ function resetLand(){
 
 //score points assigned to each vegetable
 var score = 0;
-$('#land').on("click", "span", function(evt){
+$('#land').on("click", "span", function(evt) {
   if (evt.target.src === apple) {
     score = score + 1;
   } else if (evt.target.src === snail) {
@@ -45,16 +49,16 @@ $('#land').on("click", "span", function(evt){
   } else if (evt.target.src === avocado) {
     score = score + 7;
   } else if (evt.target.src === watermelon) {
-   score = score + 6;
- } else {
-   console.log('no match');
- };
+    score = score + 6;
+  } else {
+    console.log('no match');
+  };
 
-$( evt.target ).hide( 500, function() {
-  $( evt.target ).remove();
-  console.log(score);
-  console.log($('#scorePoints').text(score)); //displays the current score
- });
+  $(evt.target).hide(500, function() {
+    $(evt.target).remove();
+    console.log(score);
+    console.log($('#scorePoints').text(score)); //displays the current score
+  });
 });
 
 var targetScore = {
@@ -63,20 +67,16 @@ var targetScore = {
   levelThree: 201,
   levelFour: 142,
   levelFive: 179,
-  levelSix:195,
-  levelSeven:209,
+  levelSix: 195,
+  levelSeven: 209,
 };
 
-//Clock countdown
-var timer;
-var minutes;
-var seconds;
-var interval;
 
 
-function clearAllIntervals () {
+
+function clearAllIntervals() {
   var lastId = setInterval(null, 5000)
-  for (var i=0; i <= lastId; i++) {
+  for (var i = 0; i <= lastId; i++) {
     clearInterval(i)
   }
 };
@@ -85,76 +85,88 @@ function startTimer(duration, display) {
   clearAllIntervals()
   timer = duration, minutes, seconds;
 
-     var interval = setInterval(function() {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      display.text(minutes + ":" + seconds);
+  var interval = setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.text(minutes + ":" + seconds);
 
-    if (score === 104 && level === "one"){
+    if (score === 104 && level === "one") {
       clearInterval(interval);
       console.log(clearInterval);
-       winLevelOne();
-    } else if (score > 104 && level === "one"){
+      winLevelOne();
+    } else if (score > 104 && level === "one") {
       clearInterval(interval);
       console.log(clearInterval);
-       youLose();
-    } if (score === 168 && level === "two"){
-       winLevelTwo();
-    } else if (score > 168 && level === "two"){
-       youLose();
-    } if (score === 201 && level === "three"){
-       winLevelThree();
-    } else if (score > 201 && level === "three"){
-       youLose();
-    } if (score === 142 && level === "four"){
-       winLevelFour();
-    } else if (score > 142 && level === "four"){
-       youLose();
-    } if (score === 179 && level === "five"){
-       winLevelFive();
-    } else if (score > 179 && level === "five"){
-       youLose();
-    } if (score === 195 && level === "six"){
-       winLevelSix();
-    } else if (score > 195 && level === "six"){
       youLose();
-    } if (score === 209 && level === "seven"){
-       winLevelSeven();
-    } else if (score > 209 && level === "seven"){
-       youLose();
+    }
+    if (score === 168 && level === "two") {
+      winLevelTwo();
+    } else if (score > 168 && level === "two") {
+      youLose();
+    }
+    if (score === 201 && level === "three") {
+      winLevelThree();
+    } else if (score > 201 && level === "three") {
+      youLose();
+    }
+    if (score === 142 && level === "four") {
+      winLevelFour();
+    } else if (score > 142 && level === "four") {
+      youLose();
+    }
+    if (score === 179 && level === "five") {
+      winLevelFive();
+    } else if (score > 179 && level === "five") {
+      youLose();
+    }
+    if (score === 195 && level === "six") {
+      winLevelSix();
+    } else if (score > 195 && level === "six") {
+      youLose();
+    }
+    if (score === 209 && level === "seven") {
+      winLevelSeven();
+    } else if (score > 209 && level === "seven") {
+      youLose();
     };
 
     if (--timer < 0) {
-     timer = 0;
-    if (score === targetScore.levelOne && level === "one"){
+      timer = 0;
+      if (score === targetScore.levelOne && level === "one") {
         winLevelOne();
-    } else if (score !== targetScore.levelOne && level === "one"){
+      } else if (score !== targetScore.levelOne && level === "one") {
         youLose();
-    } if (score === targetScore.levelTwo && level === "two"){
+      }
+      if (score === targetScore.levelTwo && level === "two") {
         winLevelTwo();
-    } else if (score !== targetScore.levelTwo && level === "two"){
+      } else if (score !== targetScore.levelTwo && level === "two") {
         youLose();
-    } if (score === targetScore.levelThree && level === "three"){
+      }
+      if (score === targetScore.levelThree && level === "three") {
         winLevelThree();
-    } else if (score !== targetScore.levelThree && level === "three"){
+      } else if (score !== targetScore.levelThree && level === "three") {
         youLose();
-    } if (score === targetScore.levelFour && level === "four"){
+      }
+      if (score === targetScore.levelFour && level === "four") {
         winLevelFour();
-    } else if (score !== targetScore.levelFour && level === "four"){
+      } else if (score !== targetScore.levelFour && level === "four") {
         youLose();
-    } if (score === targetScore.levelFive && level === "five"){
+      }
+      if (score === targetScore.levelFive && level === "five") {
         winLevelFive();
-    } else if (score !== targetScore.levelFive && level === "five"){
+      } else if (score !== targetScore.levelFive && level === "five") {
         youLose();
-    } if (score === targetScore.levelSix && level === "six"){
+      }
+      if (score === targetScore.levelSix && level === "six") {
         winLevelSix();
-    } else if (score !== targetScore.levelSix && level === "six"){
+      } else if (score !== targetScore.levelSix && level === "six") {
         youLose();
-    } if (score === targetScore.levelSeven && level === "seven"){
+      }
+      if (score === targetScore.levelSeven && level === "seven") {
         winLevelSeven();
-    } else if (score !== targetScore.levelSeven && level === "seven"){
+      } else if (score !== targetScore.levelSeven && level === "seven") {
         youLose();
       };
     };
@@ -164,13 +176,13 @@ function startTimer(duration, display) {
 display = $('.timer');
 
 // instructions window
-  $('#zero').click(function() {
+$('#zero').click(function() {
   $('#instructions').hide();
   levelOne();
 });
 
 //popup level 1
-function levelOne(){
+function levelOne() {
   $('#popupOne').show();
   $('#first').click(function() {
     $('#popupOne').hide();
@@ -184,9 +196,9 @@ function levelOne(){
 };
 
 //When player loses, must restart to Level 1!
-function youLose(){
+function youLose() {
   $('.restartOne').show();
-  $('#restart').click(function(){
+  $('#restart').click(function() {
     $('.restartOne').hide();
     score = 0;
     levelOne();
@@ -228,6 +240,11 @@ function winLevelThree() {
   $('#four').click(function() {
     $('#popupFour').hide();
     startTimer(40, display);
+    test = [];
+    resetLand();
+    level = "three";
+    score = 0;
+    farm();
   });
 };
 
@@ -237,6 +254,11 @@ function winLevelFour() {
   $('#five').click(function() {
     $('#popupFive').hide();
     startTimer(30, display);
+    test = [];
+    resetLand();
+    level = "three";
+    score = 0;
+    farm();
   });
 };
 
@@ -246,6 +268,11 @@ function winLevelFive() {
   $('#six').click(function() {
     $('#popupSix').hide();
     startTimer(25, display);
+    test = [];
+    resetLand();
+    level = "three";
+    score = 0;
+    farm();
   });
 };
 
@@ -255,6 +282,11 @@ function winLevelSix() {
   $('#seven').click(function() {
     $('#popupSeven').hide();
     startTimer(20, display);
+    test = [];
+    resetLand();
+    level = "three";
+    score = 0;
+    farm();
   });
 };
 
@@ -264,6 +296,11 @@ function winLevelSeven() {
   $('#win').click(function() {
     $('#popupWin').hide();
     levelOne();
+    test = [];
+    resetLand();
+    level = "three";
+    score = 0;
+    farm();
   });
 };
 
